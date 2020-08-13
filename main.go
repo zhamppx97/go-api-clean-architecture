b61f93391a2b6dd489d3cd1fad30132b965586dc
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-13 15:55:30 +0700

    Update

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/labstack/echo"
+	"github.com/zhamppx97/go-api-clean-architecture/api/v1/customer/di"
+	"github.com/zhamppx97/go-api-clean-architecture/api/v1/customer/gateway/route"
+)
+
 func main() {
 	e := echo.New()
 	// Routes
