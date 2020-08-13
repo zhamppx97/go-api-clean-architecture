674a141e4f34ad1bdaea36ee9f221faec697ec73
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-08-13 17:01:06 +0700

    Update

--- customer/api/v1/customer/model/customer.go
+++ customer/api/v1/customer/model/customer.go
@@ -2,7 +2,7 @@ package model
 
 // Customer model
 type Customer struct {
-    ID          int           `json:"id" db:"id"`
-	Code        string        `json:"code" db:"code"`
-	Name        string        `json:"name" db:"name"`
+    ID int `json:"id" db:"id"`
+	Code string `json:"code" db:"code"`
+	Name string `json:"name" db:"name"`
 }
